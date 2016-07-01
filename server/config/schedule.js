3c372f6895324f46fb7299dc58c2a3227d4e9559
Avrohom David Gelbfish <adg@-------.com>
2016-07-01 00:24:49 -0400

    removed pre-start and fixed the build

--- server/config/schedule.js
+++ server/config/schedule.js
@@ -2,6 +2,6 @@ import later from 'later'
 
 later.date.localTime();
 
-const schedule = later.parse.text('on monday, tuesday, wednesday, thursday at 1:45 am');
+const schedule = later.parse.text('on monday, tuesday, wednesday, thursday at 1:44 pm');
 
 export default schedule
